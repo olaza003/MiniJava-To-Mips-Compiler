04e4d9009df5cde457a642e0abd0e2d84639b0a8
derek-le1105 <derekle1105@gmail.com>
2022-08-08 13:17:51 -0700

    Updated necessary files

--- V2VM.java
+++ V2VM.java
@@ -1,14 +1,22 @@
+import cs132.util.ProblemException;
+import cs132.vapor.parser.VaporParser;
+import cs132.vapor.ast.VaporProgram;
+import cs132.vapor.ast.VBuiltIn.Op;
 
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.io.InputStream;
-import syntaxtree.*;
+
+
 
 public class V2VM {
     public static void processStream(InputStream stream){
         try{
-            MiniJavaParser parser = new MiniJavaParser(stream);
-            Goal root = parser.Goal();
+            //MiniJavaParser parser = new MiniJavaParser(stream);
+            //Goal root = parser.Goal();
             //pass typecheck = new pass(root);
-            //System.out.println("done");
+            System.out.println("done");
         }
         catch (Exception e){
             System.out.println("Type error");
@@ -21,4 +29,28 @@ public class V2VM {
     public static void main(String args[]){
         processStream(System.in);
     }
+
+
+    public static VaporProgram parseVapor(InputStream in, PrintStream err) throws IOException {
+        Op[] ops = {
+          Op.Add, Op.Sub, Op.MulS, Op.Eq, Op.Lt, Op.LtS,
+          Op.PrintIntS, Op.HeapAllocZ, Op.Error,
+        };
+        boolean allowLocals = true;
+        String[] registers = null;
+        boolean allowStack = false;
+      
+        VaporProgram tree;
+        try {
+          tree = VaporParser.run(new InputStreamReader(in), 1, 1,
+                                 java.util.Arrays.asList(ops),
+                                 allowLocals, registers, allowStack);
+        }
+        catch (ProblemException ex) {
+          err.println(ex.getMessage());
+          return null;
+        }
+      
+        return tree;
+      }
 }
