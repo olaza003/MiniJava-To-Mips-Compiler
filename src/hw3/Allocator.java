06a20e3adccf2710318dc9b60b82bbbd68855fa6
derek-le1105 <derekle1105@gmail.com>
2022-08-15 11:43:44 -0700

    Implemented initial sorting by start range for interval ranges

--- src/hw3/Allocator.java
+++ src/hw3/Allocator.java
@@ -5,10 +5,39 @@ import cs132.vapor.ast.VVarRef;
 import java.util.List;
 
 public class Allocator {
+    List<String> activeList;
 
     public AllocationMap computeAllocation(List<IntervalNode> intervals, VVarRef.Local[] params){
+        List<String> active;
+        sortStartIntervalList(intervals);
+
+
+        for(IntervalNode i: intervals){
+            expireOldIntervals(i);
+        }
+
 
         AllocationMap temp = new AllocationMap();
         return temp;
+    }
+
+    public void expireOldIntervals(IntervalNode i){
+
+    }
+
+    public void sortStartIntervalList(List<IntervalNode> intervals){
+        for(int i = 0; i < intervals.size(); i++){
+            IntervalNode min = intervals.get(i);
+            int minId = i;
+            for(int j = i + 1; j < intervals.size(); j++){
+                if(intervals.get(j).start < min.start){
+                    min = intervals.get(j);
+                    minId = j;
+                }
+            }
+            IntervalNode temp = intervals.get(i);
+            intervals.set(i, min);
+            intervals.set(minId, temp);
+        }
     }
 }
