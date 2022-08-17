ead93341fc66cb0b145318070e69bebd4bc9c020
derek-le1105 <derekle1105@gmail.com>
2022-08-16 21:34:29 -0700

    Working on output for visits

---
+++ src/hw3/OutputVisit.java
@@ -0,0 +1,57 @@
+package hw3;
+
+import cs132.vapor.ast.*;
+
+public class OutputVisit extends VInstr.VisitorR<String, RuntimeException> {
+
+    public AllocationMap map;
+
+    public OutputVisit(AllocationMap m){
+        map = m;
+    }
+
+    @Override
+    public String visit(VAssign vAssign) throws RuntimeException {
+
+        Register varReg = map.registerHashMap.get(vAssign.dest.toString());
+        String LHS = varReg.register;
+        String RHS = (vAssign.source instanceof  VVarRef) ? map.registerHashMap.get(vAssign.source.toString()).register : vAssign.source.toString();
+
+        return LHS + " = " + RHS;
+    }
+
+    @Override
+    public String visit(VCall vCall) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public String visit(VBuiltIn vBuiltIn) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public String visit(VMemWrite vMemWrite) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public String visit(VMemRead vMemRead) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public String visit(VBranch vBranch) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public String visit(VGoto vGoto) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public String visit(VReturn vReturn) throws RuntimeException {
+        return null;
+    }
+}
