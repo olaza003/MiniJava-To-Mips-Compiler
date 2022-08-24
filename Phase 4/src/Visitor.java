75d4e249d7ab7887eb5860c21fd56a3a5cd07004
derek-le1105 <derekle1105@gmail.com>
2022-08-24 00:51:23 -0700

    Setting up prints for visit ouputs

--- Phase 4/src/Visitor.java
+++ Phase 4/src/Visitor.java
@@ -2,49 +2,49 @@ import cs132.vapor.ast.*;
 public class Visitor extends VInstr.VisitorR<String, RuntimeException>{
     @Override
     public String visit(VAssign vAssign) throws RuntimeException {
-        System.out.println("VAssign");
-        return null;
+        //System.out.println("VAssign");
+        return "vAssign";
     }
 
     @Override
     public String visit(VCall vCall) throws RuntimeException {
-        System.out.println("VCall");
-        return null;
+        //System.out.println("VCall");
+        return "vCall";
     }
 
     @Override
     public String visit(VBuiltIn vBuiltIn) throws RuntimeException {
-        System.out.println("VBuiltin");
-        return null;
+        //System.out.println("VBuiltin");
+        return "vBuiltIn";
     }
 
     @Override
     public String visit(VMemWrite vMemWrite) throws RuntimeException {
-        System.out.println("VMemWrite");
-        return null;
+        //System.out.println("VMemWrite");
+        return "vMemWrite";
     }
 
     @Override
     public String visit(VMemRead vMemRead) throws RuntimeException {
-        System.out.println("VMemRead");
-        return null;
+        //System.out.println("VMemRead");
+        return "vMemRead";
     }
 
     @Override
     public String visit(VBranch vBranch) throws RuntimeException {
-        System.out.println("VBranch");
-        return null;
+        //System.out.println("VBranch");
+        return "vBranch";
     }
 
     @Override
     public String visit(VGoto vGoto) throws RuntimeException {
-        System.out.println("VGoto");
-        return null;
+        //System.out.println("VGoto");
+        return "vGoto";
     }
 
     @Override
     public String visit(VReturn vReturn) throws RuntimeException {
-        System.out.println("VReturn");
-        return null;
+        //System.out.println("VReturn");
+        return "vReturn";
     }
 }
