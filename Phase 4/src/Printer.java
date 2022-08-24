7adb993b416248ffd2306ee117571a91daf488fc
olaza003 <olaza003@ucr.edu>
2022-08-23 17:56:36 -0700

    push printer

---
+++ Phase 4/src/Printer.java
@@ -0,0 +1,25 @@
+import cs132.vapor.ast.VDataSegment;
+
+public class Printer {
+    public String dataSeg = "";
+    private static final String IDENT = "  ";
+    private String ident = "";
+    //handles the extra like .data, class with its methods or dataSegments
+    public String printDataSeg(VDataSegment[] cov){
+        dataSeg += ".data\n\n";
+        increaseIdent();
+        for(int i = 0; i < cov.length; i++){
+            dataSeg += cov[i].ident + ":\n";
+            for(int k = 0; k < cov[i].values.length; k++){
+                dataSeg += ident + cov[i].values[k].toString().substring(1) + "\n";
+            }
+            dataSeg += "\n";
+        }
+        decreaseIdent();
+        dataSeg += "\n" + ident + ".text\n";
+        return dataSeg;
+    }
+
+    public void increaseIdent(){ident += IDENT;}
+    public void decreaseIdent(){ident = ident.substring(0, ident.length() - IDENT.length());}
+}
